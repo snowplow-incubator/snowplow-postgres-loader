f5b60931b189574e5a78a5ff342a627b0747bcb7
Anton Parkhomenko <mailbox@chuwy.me>
2020-06-18 02:17:28 +0700

    Try GH actions

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,23 @@
+<!--
+
+** Please read the guidelines below. **
+
+GitHub issues are reserved for bug reports.
+The best place to ask general questions or ask for community support is
+[on our Discourse](https://discourse.snowplowanalytics.com/).
+
+-->
+
+**Version**:
+
+**Expected behavior**:
+
+**Actual behavior**:
+
+**Steps to reproduce**:
+
+<!-- Please try to be as detailed as possible so that we can reproduce and fix the issue
+as quickly as possible. -->
+ 1.
+ 2.
+
--- .travis/start_environment.sh
+++ .travis/start_environment.sh
@@ -2,38 +2,38 @@
 
 set -e
 
-if [ -z ${TRAVIS_BUILD_DIR+x} ]; then 
-    echo "TRAVIS_BUILD_DIR is unset"; 
+if [ -z ${GITHUB_WORKSPACE+x} ]; then
+    echo "GITHUB_WORKSPACE is unset";
     exit 1
 fi
 
 IGLUCTL_ZIP="igluctl_0.6.0.zip"
 IGLUCTL_URI="http://dl.bintray.com/snowplow/snowplow-generic/$IGLUCTL_ZIP"
-IGLUCENTRAL_PATH="$TRAVIS_BUILD_DIR/iglu-central"
+IGLUCENTRAL_PATH="$GITHUB_WORKSPACE/iglu-central"
 SCHEMAS_PATH="$IGLUCENTRAL_PATH/schemas/" 
-TEST_SCHEMAS="$TRAVIS_BUILD_DIR/.travis/schemas/"
+TEST_SCHEMAS="$GITHUB_WORKSPACE/.github/schemas/"
 POSTGRES_PASSWORD=mysecretpassword
 
-# git clone https://github.com/snowplow/iglu-central.git $IGLUCENTRAL_PATH
-# 
-# docker run \
-#     -p 8080:8080 \
-#     -v $TRAVIS_BUILD_DIR/.travis:/iglu \
-#     --rm -d \
-#     snowplow-docker-registry.bintray.io/snowplow/iglu-server:0.6.0 \
-#     --config /iglu/server.conf
-# 
-# wget $IGLUCTL_URI
-# unzip -j $IGLUCTL_ZIP
-# 
-# echo "Waiting for Iglu Server..."
-# sleep 6
-# 
-# ./igluctl static push \
-#     $SCHEMAS_PATH \
-#     http://localhost:8080/ \
-#     48b267d7-cd2b-4f22-bae4-0f002008b5ad \
-#     --public
+git clone https://github.com/snowplow/iglu-central.git $IGLUCENTRAL_PATH
+
+docker run \
+    -p 8080:8080 \
+    -v $GITHUB_WORKSPACE/.github:/iglu \
+    --rm -d \
+    snowplow-docker-registry.bintray.io/snowplow/iglu-server:0.6.0 \
+    --config /iglu/server.conf
+
+wget $IGLUCTL_URI
+unzip -j $IGLUCTL_ZIP
+
+echo "Waiting for Iglu Server..."
+sleep 6
+
+./igluctl static push \
+    $SCHEMAS_PATH \
+    http://localhost:8080/ \
+    48b267d7-cd2b-4f22-bae4-0f002008b5ad \
+    --public
 
 ./igluctl static push \
     $TEST_SCHEMAS \
@@ -41,15 +41,4 @@ POSTGRES_PASSWORD=mysecretpassword
     48b267d7-cd2b-4f22-bae4-0f002008b5ad \
     --public
 
-
-# docker run \
-#     -p 5432:5432 \
-#     --rm \
-#     --name pg-loader-box \
-#     -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-#     -d postgres
-# 
-# echo "Waiting for Postgres..."
-# sleep 3
-# 
-# PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U postgres -c "CREATE DATABASE snowplow"
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U postgres -c "CREATE DATABASE snowplow"
