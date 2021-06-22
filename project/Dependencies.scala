9da9363299016381083559904f8bb16b2c8529a2
Ian Streeter <ian@snowplowanalytics.com>
2021-06-22 21:15:42 +0100

    Bump fs2-google-pubsub to 0.17.0 (close #23)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val circe        = "0.14.1"
     val circeConfig  = "0.8.0"
     val fs2Aws       = "3.1.1"
-    val fs2PubSub    = "0.16.1"
+    val fs2PubSub    = "0.17.0"
     val doobie       = "0.9.2"
     val fs2          = "2.4.4"
     val log4s        = "1.8.2"
