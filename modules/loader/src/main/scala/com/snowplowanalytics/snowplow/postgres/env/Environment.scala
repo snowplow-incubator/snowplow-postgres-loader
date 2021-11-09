82a0ca5bfa6ce7a392df2c6d62493d5d6d2638f2
Ian Streeter <ian@snowplowanalytics.com>
2021-11-09 21:02:54 +0000

    Use direct executor for KPL callback (close #73)

--- modules/loader/src/main/scala/com/snowplowanalytics/snowplow/postgres/env/Environment.scala
+++ modules/loader/src/main/scala/com/snowplowanalytics/snowplow/postgres/env/Environment.scala
@@ -43,7 +43,7 @@ object Environment {
     for {
       badSink <- config.output.bad match {
         case LoaderConfig.StreamSink.Noop(period) => DummyStreamSink.create(period)
-        case c: LoaderConfig.StreamSink.Kinesis => KinesisSink.create(c, config.monitoring, config.backoffPolicy, blocker)
+        case c: LoaderConfig.StreamSink.Kinesis => KinesisSink.create(c, config.monitoring, config.backoffPolicy)
         case c: LoaderConfig.StreamSink.PubSub => PubSubSink.create(c, config.backoffPolicy)
         case c: LoaderConfig.StreamSink.Local => LocalSink.create(c, blocker)
       }
